7bad48320cd3556f20800e083f65dae6966bac07
snowdog <snowdog@biccotakko.link>
2021-05-09 23:21:31 +0900

    First Commit

---
+++ src/js/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+}))
+
+export const App = () => {
+  const classes = useStyles()
+
+  return (
+    <>Hello world</>
+  )
+}
